d71c4d1eb1a1493ff8e1e329a3d968b8de3b5d91
Aditya Goel <adityagoelgithub0320@gmail.com>
2024-06-15 18:33:18 +0530

    made a change for deplyment isssue

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -72,7 +72,7 @@ const router = createBrowserRouter([
         element: <Orders />,
       },
       {
-        path: "success",
+        path: "/success/:sessionId",
         element: <Success />,
       },
       {
