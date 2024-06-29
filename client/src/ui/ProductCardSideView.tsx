5d2f15cb53499dfa0afa5039a8008a96a4d3e1d4
Aditya Goel <adityagoelgithub0320@gmail.com>
2024-06-29 23:20:40 +0530

    extra import removed

--- client/src/ui/ProductCardSideView.tsx
+++ client/src/ui/ProductCardSideView.tsx
@@ -1,27 +1,10 @@
-import { store } from "../lib/store";
-import { ProductProps } from "../../type";
-import { useEffect, useState } from "react";
+// import React from 'react'
 
-const ProductCardSideView = ({ product }: { product?: ProductProps }) => {
-  const { addToFavorite, favoriteProduct } = store();
-
-  const [existingProduct, setExistingProduct] = useState<ProductProps | null>(
-    null
-  );
-
-  useEffect(() => {
-    const availableItem = favoriteProduct.find(
-      (item) => item?._id === product?._id
-    );
-    setExistingProduct(availableItem || null);
-  }, [product, favoriteProduct]);
-
-
+export const ProductCardSideView = () => {
   return (
-    <div className="absolute right-1 top-1 flex flex-col gap-1 transition translate-x-12 group-hover:translate-x-0 duration-300">
-      
-    </div>
-  );
-};
+    // <div>ProductCardSideView</div>
+    <>
 
-export default ProductCardSideView;
+    </>
+  )
+}
